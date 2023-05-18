22bb6bef58a680c245aebec44f684ac4d527d40f
Baitur <tawbaev777@gmail.com>
2023-05-18 13:17:06 +0600

    update 2

--- src/main/java/com/example/prototip/repo/AppRepository.java
+++ src/main/java/com/example/prototip/repo/AppRepository.java
@@ -1,7 +1,6 @@
 package com.example.prototip.repo;
 
 import com.example.prototip.models.App;
-import com.example.prototip.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
